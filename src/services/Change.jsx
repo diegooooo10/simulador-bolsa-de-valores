2cfe87c62bf7703532dab90b86cedf3fcda9a57c
diegooooo10 <diegoel.valencia@gmail.com>
2024-12-10 18:04:39 -0600

    .

--- src/services/Change.jsx
+++ src/services/Change.jsx
@@ -1,11 +1,11 @@
 import { TrendingDown, TrendingUp } from "./svg";
 
 export const Change = ({ change }) => {
-  const changeClass = change >= 0 ? "text-green-500" : "text-red-400";
+  const changeClass = change >= 0 ? "text-green-500" : "text-red-600";
   const changeIcon = change >= 0 ? <TrendingUp /> : <TrendingDown />;
   return (
     <div className={`flex items-center justify-center ${changeClass}`}>
-      <div className="mr-1">{changeIcon}</div>
+      <div className="mr-1 ">{changeIcon}</div>
       <div>{change}%</div>
     </div>
   );
--- src/services/TIempo.jsx
+++ src/services/TIempo.jsx
@@ -1,9 +1,9 @@
 const date = new Date();
 export const Tiempo = () => {
-  return <p class="text-gray-400 text-sm">{date.toLocaleString()}</p>
+  return <p className="text-gray-400 text-sm">{date.toLocaleString()}</p>
 }
 
 export const BuyDate = () => {
-  return <td class="px-6 py-3">{date.toLocaleDateString()}</td>
+  return <td className="px-6 py-3">{date.toLocaleDateString()}</td>
 }
 
